2f95d27ecca9618e6c1a4413338c39b3dbfc3b9a
Seth <sethgoldin27@gmail.com>
2020-08-02 19:32:21 -0400

    clean up js

--- js/main.js
+++ js/main.js
@@ -10,18 +10,9 @@ function send_mail() {
 (function ($) {
 	"use strict";
 
-	//1.Hide Loading Box (Preloader)
-	function handlePreloader() {
-		if ($(".preloader").length) {
-			$(".preloader").delay(200).fadeOut(500);
-		}
-	}
-
-	//6.Scroll to a Specific Div
 	if ($(".scroll-to-target").length) {
 		$(".scroll-to-target").click("click", function () {
 			var target = $(this).attr("data-target");
-			// animate
 			$("html, body").animate(
 				{
 					scrollTop: $(target).offset().top
@@ -31,51 +22,16 @@ function send_mail() {
 		});
 	}
 
-	//28.Elements Animation
 	if ($(".wow").length) {
 		var wow = new WOW({
-			boxClass: "wow", // animated element css class (default is wow)
-			animateClass: "animated", // animation css class (default is animated)
-			offset: 0, // distance to the element when triggering the animation (default is 0)
-			mobile: false, // trigger animations on mobile devices (default is true)
-			live: true // act on asynchronously loaded content (default is true)
+			boxClass: "wow",
+			animateClass: "animated",
+			offset: 0,
+			mobile: false,
+			live: true
 		});
 		wow.init();
 	}
-	// Testimonial Slider
-	// $(".testimonial-carousel").slick({
-	// 	infinite: true,
-	// 	arrows: false,
-	// 	autoplay: true,
-	// 	slidesToShow: 2,
-	// 	dots: true,
-	// 	slidesToScroll: 2,
-	// 	responsive: [
-	// 		{
-	// 			breakpoint: 991,
-	// 			settings: {
-	// 				slidesToShow: 2
-	// 			}
-	// 		},
-	// 		{
-	// 			breakpoint: 525,
-	// 			settings: {
-	// 				slidesToShow: 1
-	// 			}
-	// 		}
-	// 	]
-	// });
-
-	// FancyBox Video
-	// $("[data-fancybox]").fancybox({
-	// 	youtube: {
-	// 		controls: 0,
-	// 		showinfo: 0
-	// 	},
-	// 	vimeo: {
-	// 		color: "f00"
-	// 	}
-	// });
 
 	$(window).scroll(function () {
 		if ($(this).scrollTop() > 500) {
@@ -93,11 +49,5 @@ function send_mail() {
 			500
 		);
 		return false;
-	});
-
-	$(window).on("load", function () {
-		(function ($) {
-			handlePreloader();
-		})(jQuery);
 	});
 })(window.jQuery);
